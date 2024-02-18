a9b56034d8c67874eeb789aa31d804cdd76566ac
SalityR <5550305sality@gmail.com>
2024-02-18 15:18:16 +0200

    sd

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
-print("hello world")
-print("hello world2")
+import math
+
+def single_lists(list_data:list):
+
+    list_by_lists = [[]]
+    for num in list_data:
+        list_by_lists.append([num])
+    return list_by_lists
+
+def neighours_lists(list_data:list):
+
+    list_by_lists = []
+    iter = 0
+    for _ in list_data:
+        if(iter + 1 < len(list_data)):
+            list_by_lists.append([list_data[iter], list_data[iter + 1]])
+        
+        iter += 1
+
+    iter = 0
+    for _ in list_data:
+        if(iter + 2 < len(list_data)):
+            list_by_lists.append([list_data[iter], list_data[iter + 1], list_data[iter + 2]])
+        
+        iter += 1
+    return list_by_lists
+
+    
+
+def data_preparation(list_data):
+    list_result = []
+    print(list_data)
+
+    list_result.extend(single_lists(list_data))
+    list_result.extend(neighours_lists(list_data))
+    list_result.extend([list_data])
+
+    print(list_result)
+
+
+    for sublist in list_data:
+        pass
+       
+    
+
+
+d = [4, 6, 1, 3]
+data_preparation(d)
